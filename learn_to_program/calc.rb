fc2b0c590f6aad21ad440ff9c069e57d0dc313ef
Charles Massry <charlesdmassry@gmail.com>
2014-05-22 18:22:15 -0400

    1.Numbers

---
+++ learn_to_program/calc.rb
@@ -0,0 +1,20 @@
+puts 1.0 + 2.0
+puts 2.0 * 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts 1+2
+puts 2*3
+puts 5-8
+puts 9/2
+
+puts "how many hours are in a year?"
+puts 24 * 365
+puts "how many minutes are in a decade?"
+puts 60 * 24 * 365 * 10
+puts "how many seconds old are you?"
+puts 26.25 * 365 * 24 * 60 * 60
+puts "how many chocolates do you hope to eat in your life?"
+puts 400 * 97
+puts "If I am 1031 million seconds old, how old am I?"
+puts ((((1031000000 / 60) / 60) / 24) / 365.25)
